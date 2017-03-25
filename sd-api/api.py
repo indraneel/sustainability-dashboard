151711366c3844de9a256726171e9f1fedbe0deb
ShaneOH <shane.ohanlon93@gmail.com>
2017-03-25 17:55:11 -0400

    separate api components

--- sd-api/api.py
+++ sd-api/api.py
@@ -1,43 +1,11 @@
 #!/usr/bin/env python3
-
 from flask import Flask, jsonify, abort, request
-from flask_sqlalchemy import SQLAlchemy
+from .tables import db, CertReports, ReportFiles
+from .errors import NotFound, MissingKey
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
-db = SQLAlchemy(app)
-
-class Table:
-    def as_json(self):
-        """Convert query result to JSON"""
-        return jsonify({c.name: getattr(self, c.name) for c in self.__table__.columns})
-
-    @staticmethod
-    def list_json(results):
-        """Convert list of query results to JSON"""
-        return jsonify([{c.name: getattr(res, c.name) for c in res.__table__.columns} for res in results])
-
-    def get_column(self, column):
-        return jsonify([col[0] for col in self.query.with_entities(column).distinct(column)])
-
-
-class CertReports(Table, db.Model):
-    __tablename__ = 'cert_reports'
-    report_id = db.Column(db.Integer, primary_key=True)
-    town = db.Column(db.VARCHAR(length=50), nullable=False)
-    action = db.Column(db.VARCHAR(length=100))
-    category = db.Column(db.VARCHAR(length=100))
-    assets = db.Column(db.Text)
-    contact = db.Column(db.Text)
-    summary = db.Column(db.Text)
-    points = db.Column(db.SMALLINT)
-    date = db.Column(db.Date)
-
-class ReportFiles(Table, db.Model):
-    __tablename__ = 'report_files'
-    file_id = db.Column(db.Integer, db.ForeignKey('cert_report.report_id'), primary_key=True)
-    report_file = db.Column(db.LargeBinary)
-
+db.init_app(app)
 
 @app.route('/report', methods = ['GET'])
 def get_reports():
@@ -54,23 +22,34 @@ def get_column(endpoint):
 
 @app.route('/action', methods = ['GET', 'POST'])
 def action():
-    if not request.args:
+    if not request.args and request.method == 'GET':
         return get_column('action')
 
+    if not request.args.get('town'):
+        raise MissingKey('town')
+
+    town = request.args.get('town')
+    towns = CertReports.query.with_entities(CertReports.town) \
+    .distinct(CertReports.town).all()
+
+    if not [t[0] for t in towns if t[0] == town]:
+        abort(404)
+
     if request.method == 'GET':
-        if not request.args.get('town'):
-            abort(404)
-
-        town = request.args.get('town')
-        towns = CertReports.query.with_entities(CertReports.town) \
-        .distinct(CertReports.town).all()
-
-        if [t[0] for t in towns if t[0] == town]:
-            return get_town_actions(town)
-        else:
-            abort(404)
+        get_town_actions(town)
     else:
-        pass
+        params = request.get_json()
+        missing = []
+        if not params['action']:
+            missing.append('action')
+        if not params['category']:
+            missing.apparend('category')
+        if missing:
+            raise MissingKey(', '.join(missing))
+        if action['id']:
+            update_town_action(town, action)
+        else:
+            add_town_action(town, action)
 
 def get_town_actions(town):
     results = CertReports.query.with_entities \
@@ -81,12 +60,15 @@ def get_town_actions(town):
     {'id': res[0], 'action': res[1], 'category': res[2]} \
     for res in results])
 
-def add_town_action(town):
+def add_town_action(town, action):
     pass
 
-def update_town_action(town):
+def update_town_action(town, action):
     pass
 
+@app.errorhandler(NotFound)
+def handle_not_found(error):
+    return error.to_json()
 
 if __name__ == '__main__':
     app.run()
